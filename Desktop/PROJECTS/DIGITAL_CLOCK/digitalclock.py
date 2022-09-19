3f0a37f155fc9e66f7dc388590a1191f3841cc52
SYCHIA <shaphanyong@gmail.comgit config --global user.email shaphanyong@gmail.com>
2022-09-19 17:58:20 +0200

    removed welcome message

--- Desktop/PROJECTS/DIGITAL_CLOCK/digitalclock.py
+++ Desktop/PROJECTS/DIGITAL_CLOCK/digitalclock.py
@@ -7,7 +7,7 @@ window = TK()
 window.title("Digital Clcock")
 window.geometry("600x300")
 window.configure(bg"steelblue)
-label=Label(window, text="Welcome!", font=("Arial black", 78,"bold") bg="steelblue"
+label=Label(window, font=("Arial black", 78,"bold") bg="steelblue"
 label.package(paddy=100)
 
 window.mainloop()
