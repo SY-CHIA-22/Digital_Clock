31166c56734a096cd84d18cf48574568622d6b35
SYCHIA <shaphanyong@gmail.comgit config --global user.email shaphanyong@gmail.com>
2022-09-19 17:49:14 +0200

    added a welcome message

--- Desktop/PROJECTS/DIGITAL_CLOCK/digitalclock.py
+++ Desktop/PROJECTS/DIGITAL_CLOCK/digitalclock.py
@@ -1,9 +1,13 @@
 from cProfile import label
 from curses import window
+from tkinter import Label
 from thinter import TK, label
 
 window = TK()
 window.title("Digital Clcock")
 window.geometry("600x300")
+window.configure(bg"steelblue)
+label=Label(window, text="Welcome!", font=("Arial black", 78,"bold") bg="steelblue"
+label.package(paddy=100)
 
 window.mainloop()
